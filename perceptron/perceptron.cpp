e5739524c070422741f8e59cad17c6b8a513b234
James Brandreth <james.brandreth@gmail.com>
2019-11-14 23:00:56 +0000

    init

--- perceptron/perceptron.cpp
+++ perceptron/perceptron.cpp
@@ -7,37 +7,13 @@
 
 using namespace std;
 
-
-class Perceptron{
-	private:
-	vector<int> weights;
-
-
-	public:
-	
-	/*
-	void train(vector<vector<int>> points, vector<bool> classes[], float learning_rate = 0.3, int iteration_limit = 100){
-		bool converged = FALSE;
-		weights.assign(points[1].size(), 0);
-		i = 0;
-		while(i<iteration_limit){
-			
-		}
-
-
-	}
-	*/
-
-	void set_weights(vector<int> w){
-		weights = w;
-	}
-
-	int infer(vector<int> point){
-
-		point.insert(1)
-		int product = inner_product(weights.begin(); weights.end(), point.begin(), 0);
-		return product;
-	}
-	
+void Perceptron::set_weights(vector<int> w){
+	weights = w;
 }
 
+int Perceptron::infer(vector<int> point){
+	point.push_back(1);
+	int product = inner_product(weights.begin(), weights.end(), point.begin(), 0);
+	return product;
+}
+	
--- perceptron/perceptron.h
+++ perceptron/perceptron.h
@@ -1,14 +1,11 @@
 // perceptron class
 
-#include <vector>
-#include <numeric>
-
 	class Perceptron {
 		private:
-		std::vector<int> weights;
+			std::vector<int> weights;
 		public:
-		void set_weights(std::vector<int>);
-		int infer(std::vector<int>);
+			void set_weights(std::vector<int>);
+			int infer(std::vector<int>);
 	};
 
 
