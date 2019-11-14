bf65ecd098da0c6dd49b59bbfe80f8cbba92708d
James Brandreth <james.brandreth@gmail.com>
2019-11-14 22:12:54 +0000

    init

--- perceptron/perceptron.cpp
+++ perceptron/perceptron.cpp
@@ -1,16 +1,43 @@
+// Perceptron
+
+#include <vector>
+#include <numeric>
+
+#include "perceptron.h"
+
+using namespace std;
+
 
 class Perceptron{
+	private:
+	vector<int> weights;
 
-	void train(int points[], bool classes[]){
-		
-	}
+
+	public:
 	
-	bool infer(int points[]){
-				
+	/*
+	void train(vector<vector<int>> points, vector<bool> classes[], float learning_rate = 0.3, int iteration_limit = 100){
+		bool converged = FALSE;
+		weights.assign(points[1].size(), 0);
+		i = 0;
+		while(i<iteration_limit){
+			
+		}
+
+
+	}
+	*/
+
+	void set_weights(vector<int> w){
+		weights = w;
 	}
 
-	int
+	int infer(vector<int> point){
+
+		point.insert(1)
+		int product = inner_product(weights.begin(); weights.end(), point.begin(), 0);
+		return product;
+	}
 	
 }
-
 
