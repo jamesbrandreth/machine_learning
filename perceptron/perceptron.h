2c74a55a996e1e54d5cc5ab31a77f6608796a4db
James Brandreth <james.brandreth@gmail.com>
2019-11-19 23:52:07 +0000

    perceptron

--- perceptron/perceptron.h
+++ perceptron/perceptron.h
@@ -1,11 +1,45 @@
 // perceptron class
 
-	class Perceptron {
-		private:
-			std::vector<int> weights;
-		public:
-			void set_weights(std::vector<int>);
-			int infer(std::vector<int>);
-	};
+using namespace std;
+
+class Perceptron {
+	private:
+
+		vector<float> weights;
+
+		float dist(vector<float> point){
+			point.push_back(1);
+			float product = inner_product(weights.begin(), weights.end(), point.begin(), 0.0);
+			return product;
+		}
+
+	public:
+
+		void set_weights(vector<float> w){
+			weights = w;
+		}
+
+		bool train(vector<vector<float>> points, float learning_rate, int iteration_limit=100){
+			bool converged = false;
+			int i=0;
+			while (i < iteration_limit){
+				for(vector<vector<float>>::iterator pt = points.begin(); pt != points.end(); pt++){
+					vector<float> point = *pt;
+					infer(point);
+				}
+				i++;
+			}
+			return false;
+		}
+
+		float infer(vector<float> point){
+			float distance = dist(point);
+			if (distance < 0) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+};
 
 
